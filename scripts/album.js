6629cd15f2846c210169d1a3168325c97ddeff7a
Kasey Markham <kaseycm@gmail.com>
2016-10-28 22:01:49 -0400

    Assignment 2-13 completed

--- scripts/album.js
+++ scripts/album.js
@@ -62,11 +62,15 @@ var setCurrentAlbum = function(album) {
 var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
-        while (currentParent.className != targetClass && currentParent.className !== null) {
+        while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
-        return currentParent;
+    } else if (currentParent.className == null) {
+        alert("No parent found");
+    } else if (currentParent.className !== 'song-item-none') {
+        alert("No parent found with that class name");
     }
+    return currentParent;
 };
 
 var getSongItem = function(element) {
