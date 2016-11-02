55e848e0dcb7520e6ac522c248e8cc91e53ccc00
Kasey Markham <kaseycm@gmail.com>
2016-11-01 20:26:56 -0400

    Checkpoint 2-16 finished

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplates = 
+var buildCollectionItemTemplate = function() {
+    var template = 
     '<div class="collection-album-container column fourth">'
     +'   <img src="assets/images/album_covers/01.png"/>'
     +'   <div class="collection-album-info caption">'
@@ -13,15 +14,13 @@ var collectionItemTemplates =
     +'   </div>'
     +'</div>'
     ;
-
-window.onload = function() {
-    // #1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    // #2
-    collectionContainer.innerHTML = '';
     
-    // #3
-    for (var i= 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplates;
-    }
-}
+    return $(template);
+};
+
+$(window).load(function() {
+    var $collectionContainer = $('.album-covers');
+    $collectionContainer.empty();
+        var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
+});
